6fd8180f4b5d06fdcb66b9c6887e3a5e202570fa
elm19 <128943656+elm19@users.noreply.github.com>
2025-06-04 22:15:55 +0100

    refactor: remove Swagger UI integration from app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_swagger_ui import get_swaggerui_blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -20,35 +19,7 @@ def create_app():
 
     db.init_app(app)
 
-    # Register Swagger UI blueprint
-    SWAGGER_URL = '/docs'  # URL for exposing Swagger UI
-    API_URL = '/static/openapi.yaml'  # Our API url (can of course be a local resource)
-
-    swaggerui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL,
-        API_URL,
-        config={
-            'app_name': "Trading Time Series Forecast API",
-            'deepLinking': True,
-            'displayOperationId': False,
-            'displayRequestDuration': True,
-            'docExpansion': 'list',
-            'showExtensions': False,
-            'showCommonExtensions': False,
-            'supportedSubmitMethods': ['get'],
-            'dom_id': '#swagger-ui',
-            'layout': 'BaseLayout',
-            # Custom CSS to hide Swagger branding
-            'customCss': '''
-                .swagger-ui .topbar { display: none }
-                .swagger-ui .info .title small { display: none }
-                .swagger-ui .info hgroup.main { margin: 0 }
-                .swagger-ui .info .title { font-size: 30px }
-            '''
-        }
-    )
-    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
-
+    # Register blueprints
     from .routes import main
     from .predict import predict
     from .model_info import model_info
